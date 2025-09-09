e7f8ef40370cb237326380a2828cf9988b5f3c06
sinsenti <sinsenti73@gmail.com>
2025-09-09 12:38:32 +0300

    Auto-push: 2025-09-09 12:38:32

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -143,7 +143,7 @@ public class SimpleJDBCRepository {
   }
 
   // Delete user by id
-  private void deleteUser(Long userId) {
+  public void deleteUser(Long userId) {
     try (Connection conn = CustomDataSource.getInstance().getConnection();
         PreparedStatement ps = conn.prepareStatement(deleteUser)) {
       ps.setLong(1, userId);
